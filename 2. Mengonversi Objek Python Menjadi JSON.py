7f71769b780107df6c18d0483f22754ca4a11e2c
rendhive <rendhivecode@gmail.com>
2025-12-01 12:10:40 +0700

    [DOCT] Sample Library Python: 2. Mengonversi Objek Python Menjadi JSON.py

---
+++ 2. Mengonversi Objek Python Menjadi JSON.py
@@ -0,0 +1,7 @@
+import json
+
+data = {'key': 'value', 'number': 42}
+json_data = json.dumps(data)
+print("JSON encoded:", json_data)
+# Fungsi: Mengonversi objek Python (dictionary) menjadi format JSON.
+# Kondisi: Ketika Anda ingin menyimpan atau mentransmisikan data dalam format JSON.
