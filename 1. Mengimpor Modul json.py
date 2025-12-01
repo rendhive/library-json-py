da1721c0f8faa710e2145abdbad53224fe383578
rendhive <rendhivecode@gmail.com>
2025-12-01 11:56:41 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul json.py

---
+++ 1. Mengimpor Modul json.py
@@ -0,0 +1,5 @@
+import json
+
+print("Modul json berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk melakukan encoding dan decoding data JSON.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari json.
