7609b33816083b22f2b430c0d7a07638cba62170
rendhive <rendhivecode@gmail.com>
2025-12-01 12:13:13 +0700

    [DOCT] Sample Library Python: 7. Mengonversi List ke JSON.py

---
+++ 7. Mengonversi List ke JSON.py
@@ -0,0 +1,8 @@
+import json
+
+data_list = [1, 2, 3, 4, 5]
+json_data = json.dumps(data_list)
+
+print("JSON encoded list:", json_data)
+# Fungsi: Mengonversi list Python menjadi format JSON.
+# Kondisi: Ketika Anda bekerja dengan struktur data list yang perlu dikonversi.
